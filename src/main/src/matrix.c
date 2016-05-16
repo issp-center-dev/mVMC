b4b098575f4fe198e6befcbde14fd0b2ba52fa09
tmisawa <misawa@solis.t.u-tokyo.ac.jp>
2016-05-16 19:07:38 +0900

    update

--- src/main/src/matrix.c
+++ src/main/src/matrix.c
@@ -86,7 +86,8 @@ int getLWork_fcomp() {
 }
 
 
-
+//==============s freal =============//
+//  CalculateMAll,  calculateMAll_child -> real variable
 
 /* Calculate PfM and InvM from qpidx=qpStart to qpEnd */
 int CalculateMAll(const int *eleIdx, const int qpStart, const int qpEnd) {
@@ -201,3 +202,123 @@ int calculateMAll_child(const int *eleIdx, const int qpStart, const int qpEnd, c
 
   return info;
 }
+//==============e freal =============//
+
+
+//==============s fcomp =============//
+/* Calculate PfM and InvM from qpidx=qpStart to qpEnd */
+int CalculateMAll_fcomp(const int *eleIdx, const int qpStart, const int qpEnd) {
+  const int qpNum = qpEnd-qpStart;
+  int qpidx;
+
+  int info = 0;
+
+  double *myBufM;
+  double *myWork;
+  int *myIWork;
+  int myInfo;
+
+  RequestWorkSpaceThreadInt(Nsize);
+  RequestWorkSpaceThreadDouble(Nsize*Nsize+LapackLWork);
+
+  #pragma omp parallel default(shared)              \
+    private(myIWork,myWork,myInfo,myBufM)
+  {
+    myIWork = GetWorkSpaceThreadInt(Nsize);
+    myBufM = GetWorkSpaceThreadDouble(Nsize*Nsize);
+    myWork = GetWorkSpaceThreadDouble(LapackLWork);
+
+    #pragma omp for private(qpidx)
+    for(qpidx=0;qpidx<qpNum;qpidx++) {
+      if(info!=0) continue;
+      
+      myInfo = calculateMAll_child(eleIdx, qpStart, qpEnd, qpidx,
+                                   myBufM, myIWork, myWork, LapackLWork);
+      if(myInfo!=0) {
+        #pragma omp critical
+        info=myInfo;
+      }
+    }
+  }
+
+  ReleaseWorkSpaceThreadInt();
+  ReleaseWorkSpaceThreadDouble();
+  return info;
+}
+
+int calculateMAll_child_fcomp(const int *eleIdx, const int qpStart, const int qpEnd, const int qpidx,
+                        double *bufM, int *iwork, double *work, int lwork) {
+  #pragma procedure serial
+  /* const int qpNum = qpEnd-qpStart; */
+  int msi,msj;
+  int rsi,rsj;
+
+  char uplo='U', mthd='P';
+  int m,n,lda,info=0;
+  double pfaff;
+
+  /* optimization for Kei */
+  const int nsize = Nsize;
+
+  const double *sltE = SlaterElm + (qpidx+qpStart)*Nsite2*Nsite2;
+  const double *sltE_i;
+
+  double *invM = InvM + qpidx*Nsize*Nsize;
+  double *invM_i;
+
+  double *bufM_i, *bufM_i2;
+
+  m=n=lda=Nsize;
+
+  /* store bufM */
+  /* Note that bufM is column-major and skew-symmetric. */
+  /* bufM[msj][msi] = -sltE[rsi][rsj] */
+  #pragma loop noalias
+  for(msi=0;msi<nsize;msi++) {
+    rsi = eleIdx[msi] + (msi/Ne)*Nsite;
+    bufM_i = bufM + msi*Nsize;
+    sltE_i = sltE + rsi*Nsite2;
+    #pragma loop norecurrence
+    for(msj=0;msj<nsize;msj++) {
+      rsj = eleIdx[msj] + (msj/Ne)*Nsite;
+      bufM_i[msj] = -sltE_i[rsj];
+    }
+  }
+
+  /* copy bufM to invM */
+  /* For Pfaffian calculation, invM is used as second buffer */
+  #pragma loop noalias
+  for(msi=0;msi<nsize*nsize;msi++) {
+    invM[msi] = bufM[msi];
+  }
+  /* calculate Pf M */
+  M_DSKPFA(&uplo, &mthd, &n, invM, &lda, &pfaff, iwork, work, &lwork, &info);
+  if(info!=0) return info;
+  if(!isfinite(pfaff)) return qpidx+1;
+  PfM[qpidx] = pfaff;
+
+  /* DInv */
+  M_DGETRF(&m, &n, bufM, &lda, iwork, &info); /* ipiv = iwork */
+  if(info!=0) return info;
+  
+  M_DGETRI(&n, bufM, &lda, iwork, work, &lwork, &info);
+  if(info!=0) return info;
+  
+  /* store InvM */
+  /* BufM is column-major, InvM is row-major */
+  #pragma loop noalias
+  for(msi=0;msi<nsize;msi++) {
+    invM_i = invM + msi*Nsize;
+    bufM_i = bufM + msi*Nsize;
+    bufM_i2 = bufM + msi;
+    for(msj=0;msj<nsize;msj++) {
+      invM_i[msj] = 0.5*(bufM_i2[msj*nsize] - bufM_i[msj]);
+      /* invM[i][j] = 0.5*(bufM[i][j]-bufM[j][i]) */
+    }
+  }
+
+  return info;
+}
+
+
+//==============e fcomp =============//
