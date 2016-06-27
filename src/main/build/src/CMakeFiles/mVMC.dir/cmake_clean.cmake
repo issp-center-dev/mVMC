52c97789cec8eed0fc73d9a5495c6c1113c780d0
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2016-06-27 11:35:19 +0900

    merge complex

---
+++ src/main/build/src/CMakeFiles/mVMC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mVMC.dir/vmcmain.c.o"
+  "CMakeFiles/mVMC.dir/sfmt/SFMT.c.o"
+  "mVMC.pdb"
+  "mVMC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mVMC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
