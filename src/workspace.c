21ea53c071fb56ff8a9c50b712997f9d2db4f48a
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2017-02-07 20:22:32 +0900

    add workspace.h

--- src/workspace.c
+++ src/workspace.c
@@ -25,57 +25,8 @@ along with this program. If not, see http://www.gnu.org/licenses/.
  *-------------------------------------------------------------
  * by Satoshi Morita
  *-------------------------------------------------------------*/
+#include "workspace.h"
 #pragma once
-int NumWorkSpaceInt;
-int *WorkSpaceInt;
-int *WorkSpaceIntNow;
-
-int NumWorkSpaceDouble;
-double *WorkSpaceDouble;
-double *WorkSpaceDoubleNow;
-
-int NumWorkSpaceComplex;
-double complex *WorkSpaceComplex;
-double complex *WorkSpaceComplexNow;
-
-int NumWorkSpaceThreadInt;
-int **WorkSpaceThreadInt;
-int **WorkSpaceThreadIntNow;
-
-int NumWorkSpaceThreadDouble;
-double **WorkSpaceThreadDouble;
-double **WorkSpaceThreadDoubleNow;
-
-int NumWorkSpaceThreadComplex;
-double complex **WorkSpaceThreadComplex;
-double complex **WorkSpaceThreadComplexNow;
-
-void initializeWorkSpaceAll();
-void FreeWorkSpaceAll();
-
-void RequestWorkSpaceInt(int n);
-int* GetWorkSpaceInt(int n);
-void ReleaseWorkSpaceInt();
-
-void RequestWorkSpaceDouble(int n);
-double* GetWorkSpaceDouble(int n);
-void ReleaseWorkSpaceDouble();
-
-void RequestWorkSpaceComplex(int n);
-double complex* GetWorkSpaceComplex(int n);
-void ReleaseWorkSpaceComplex();
-
-void RequestWorkSpaceThreadInt(int n);
-int* GetWorkSpaceThreadInt(int n);
-void ReleaseWorkSpaceThreadInt();
-
-void RequestWorkSpaceThreadDouble(int n);
-double* GetWorkSpaceThreadDouble(int n);
-void ReleaseWorkSpaceThreadDouble();
-
-void requestWorkSpaceThreadComplex(int n);
-double complex* GetWorkSpaceThreadComplex(int n);
-void ReleaseWorkSpaceThreadComplex();
 
 void initializeWorkSpaceAll() {
   int i;
