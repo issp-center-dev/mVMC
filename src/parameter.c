d7358312bf7be1e16484c347efed681c3a40a52d
Yuichi Motoyama <yomichi@tsg.jp>
2017-02-10 14:10:12 +0900

    [WIP] initialize variational parameters as complex value (if complex mode)

--- src/parameter.c
+++ src/parameter.c
@@ -46,7 +46,9 @@ void InitParameter() {
   for(i=0;i<NSlater;i++){
     if(OptFlag[2*i+2*NProj] > 0){ //TBC
       Slater[i] =  1*genrand_real2(); /* uniform distribution [0,1) */
-      //Slater[i] += 1*I*genrand_real2(); /* uniform distribution [0,1) */
+      if(AllComplexFlag != 0){
+        Slater[i] += 1*I*genrand_real2(); /* uniform distribution [0,1) */
+      }
       //printf("DEBUG: i=%d slater=%lf %lf \n",i,creal(Slater[i]),cimag(Slater[i]));
     } else {
       Slater[i] = 0.0;
