2a79dde44d33d16acce7b45c500a54fbf44a920d
mitsuaki1987 <kawamitsuaki@gmail.com>
2017-03-12 17:14:56 +0900

    #88 Modify version ID

--- src/include/version.h
+++ src/include/version.h
@@ -27,8 +27,8 @@ along with this program. If not, see http://www.gnu.org/licenses/.
 
 /* Semantic Versioning http://semver.org */
 /* <major>.<minor>.<patch>-<prerelease> */
-#define VERSION_MAJOR  1
-#define VERSION_MINOR  0
+#define VERSION_MAJOR  0
+#define VERSION_MINOR  2
 #define VERSION_PATCH  0
 #define VERSION_PRERELEASE  "" /* "alpha", "beta.1", etc. */
 
