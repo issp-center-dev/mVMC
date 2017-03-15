d74dca406aa7761c4b3e1bce2aef0c7e0fa85355
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2017-03-15 16:07:46 +0900

    #81 change type of initial values of fij from real to complex in complex ver.

--- src/parameter.c
+++ src/parameter.c
@@ -42,17 +42,29 @@ void InitParameter() {
 
   #pragma omp parallel for default(shared) private(i)
   for(i=0;i<NProj;i++) Proj[i] = 0.0;
-
-  for(i=0;i<NSlater;i++){
-    if(OptFlag[2*i+2*NProj] > 0){ //TBC
-      Slater[i] =  1*genrand_real2(); /* uniform distribution [0,1) */
-      //Slater[i] += 1*I*genrand_real2(); /* uniform distribution [0,1) */
-      //printf("DEBUG: i=%d slater=%lf %lf \n",i,creal(Slater[i]),cimag(Slater[i]));
-    } else {
-      Slater[i] = 0.0;
+  if(AllComplexFlag==0){
+    for(i=0;i<NSlater;i++){
+      if(OptFlag[2*i+2*NProj] > 0){ //TBC
+        Slater[i] =  1*genrand_real2(); /* uniform distribution [0,1) */
+        //Slater[i] += 1*I*genrand_real2(); /* uniform distribution [0,1) */
+        //printf("DEBUG: i=%d slater=%lf %lf \n",i,creal(Slater[i]),cimag(Slater[i]));
+      } else {
+        Slater[i] = 0.0;
+      }
     }
   }
-
+  else{
+    for(i=0;i<NSlater;i++){
+      if(OptFlag[2*i+2*NProj] > 0){ //TBC
+        Slater[i] =  1*genrand_real2(); /* uniform distribution [0,1) */
+        Slater[i] += 1*I*genrand_real2(); /* uniform distribution [0,1) */
+        Slater[i] /=sqrt(2.0);
+      } else {
+        Slater[i] = 0.0;
+      }
+    }
+ 
+  }
   for(i=0;i<NOptTrans;i++){
     OptTrans[i] = ParaQPOptTrans[i];
   }
