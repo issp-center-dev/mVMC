0c4c99ea3bbd85adf3d0f2c96d45005e518ad302
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2017-05-12 11:48:50 +0900

    #119 ignore pragma warning for CMAKE in Release mode(Default).

--- config/sekirei.cmake
+++ config/sekirei.cmake
@@ -1,6 +1,7 @@
 # for Intel Compiler
 set(CMAKE_C_COMPILER "icc" CACHE STRING "" FORCE)
-set(CMAKE_C_FLAGS "-O3 -DNDEBUG -xCORE-AVX2 -mcmodel=large -shared-intel" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG -xCORE-AVX2 -mcmodel=large -shared-intel -Wno-unknown-pragmas" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "-g -O0 -Wall  -Wformat -Werror=format-security")
 
 set(CMAKE_Fortran_COMPILER "ifort" CACHE STRING "" FORCE)
 set(CMAKE_Fortran_FLAGS "-O3 -DNDEBUG -xCORE-AVX2 -mcmodel=large -shared-intel" CACHE STRING "" FORCE)
