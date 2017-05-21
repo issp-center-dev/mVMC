78979eb8d95ffb04b5cda43d08710822d18a5103
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2017-05-21 15:46:50 +0900

    #149 modify to set Libiraries path

--- config/sekirei.cmake
+++ config/sekirei.cmake
@@ -7,7 +7,9 @@ set(CMAKE_Fortran_COMPILER "ifort" CACHE STRING "" FORCE)
 set(CMAKE_Fortran_FLAGS "-O3 -DNDEBUG -xCORE-AVX2 -mcmodel=large -shared-intel" CACHE STRING "" FORCE)
 
 if(USE_SCALAPACK)
-  set(SCALAPACK_LIBRARIES "\${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_blacs_sgimpt_lp64")
+  if(SCALAPACK_LIBRARIES MATCHES "")
+    set(SCALAPACK_LIBRARIES "\${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_blacs_sgimpt_lp64")
+  endif(SCALAPACK_LIBRARIES)
 endif(USE_SCALAPACK)
 
 # for Intel MKL
