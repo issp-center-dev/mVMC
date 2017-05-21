bc4b4b804694c91c1b40d57506526012d73ec294
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2017-05-21 15:39:40 +0900

    add optioin to use scalapack

--- config/sekirei.cmake
+++ config/sekirei.cmake
@@ -6,5 +6,9 @@ set(CMAKE_C_FLAGS_DEBUG "-g -O0 -Wall  -Wformat -Werror=format-security")
 set(CMAKE_Fortran_COMPILER "ifort" CACHE STRING "" FORCE)
 set(CMAKE_Fortran_FLAGS "-O3 -DNDEBUG -xCORE-AVX2 -mcmodel=large -shared-intel" CACHE STRING "" FORCE)
 
+if(USE_SCALAPACK)
+  set(SCALAPACK_LIBRARIES "\${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_blacs_sgimpt_lp64")
+endif(USE_SCALAPACK)
+
 # for Intel MKL
 set(BLA_VENDOR "Intel10_64lp" CACHE STRING "" FORCE)
