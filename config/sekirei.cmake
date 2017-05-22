e38081ba0855c98a67db1c4952251734b98bf87e
Yuichi Motoyama <yomichi@tsg.jp>
2017-05-22 11:43:38 +0900

    update CMakeLists.txt

--- config/sekirei.cmake
+++ config/sekirei.cmake
@@ -7,11 +7,8 @@ set(CMAKE_Fortran_COMPILER "ifort" CACHE STRING "" FORCE)
 set(CMAKE_Fortran_FLAGS "-O3 -DNDEBUG -xCORE-AVX2 -mcmodel=large -shared-intel" CACHE STRING "" FORCE)
 
 if(USE_SCALAPACK)
-  if(SCALAPACK_LIBRARY_DIR MATCHES "")
-    set(SCALAPACK_LIBRARY_DIR "$ENV{MKLROOT}/lib/intel64")
-  endif(SCALAPACK_LIBRARY_DIR MATCHES "")
   if(SCALAPACK_LIBRARIES MATCHES "")
-    set(SCALAPACK_LIBRARIES "-lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_blacs_sgimpt_lp64")
+    set(SCALAPACK_LIBRARIES "-L$ENV{MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lmkl_blacs_sgimpt_lp64")
   endif(SCALAPACK_LIBRARIES MATCHES "")
 
   message(STATUS "SCALAPACK_LIBRARY_DIR is ${SCALAPACK_LIBRARY_DIR}")
