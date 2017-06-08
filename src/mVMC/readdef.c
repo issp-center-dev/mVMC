11c7f1d55b347043eb8bceff4110fb33c4a84515
k-yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2017-06-08 13:29:18 +0900

    change function name for reading pairhop file

--- src/mVMC/readdef.c
+++ src/mVMC/readdef.c
@@ -54,7 +54,7 @@ int GetLocSpinInfo(FILE *fp, int *ArrayIdx, int Nsite, char *defname);
 
 int GetInfoCoulombIntra(FILE *fp, int *ArrayIdx, double *ArrayValue, int Nsite, int NArray, char *defname);
 
-int GetInfoPairHop(FILE *fp, int **ArrayIdx, double *ArrayValue, int Nsite, int NArray, char *defname);
+int ReadPairHopValue(FILE *fp, int **ArrayIdx, double *ArrayValue, int Nsite, int NArray, char *defname);
 
 int ReadPairDValue(FILE *fp, int **ArrayIdx, double *ArrayValue, int Nsite, int NArray, char *defname);
 
@@ -776,7 +776,7 @@ int ReadDefFileIdxPara(char *xNameListFile, MPI_Comm comm) {
           break;
 
         case KWPairHop: /*pairhop.def---------------------------------------*/
-          if (GetInfoPairHop(fp, PairHopping, ParaPairHopping, Nsite, NPairHopping, defname) != 0) info = 1;
+          if (ReadPairHopValue(fp, PairHopping, ParaPairHopping, Nsite, NPairHopping, defname) != 0) info = 1;
           break;
 
         case KWGutzwiller: /*gutzwilleridx.def---------------------------------*/
@@ -1679,7 +1679,7 @@ int GetInfoCoulombIntra(FILE *fp, int *ArrayIdx, double *ArrayValue, int Nsite, 
   return info;
 }
 
-int GetInfoPairHop(FILE *fp, int **ArrayIdx, double *ArrayValue, int Nsite, int NArray, char *defname) {
+int ReadPairHopValue(FILE *fp, int **ArrayIdx, double *ArrayValue, int Nsite, int NArray, char *defname) {
   char ctmp2[256];
   int idx = 0, info = 0;
   int x0 = 0, x1 = 0;
