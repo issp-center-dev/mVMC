2df338f7a43eefabfccaed00c65e4250d1527008
Kazuyoshi Yoshimi <k-yoshimi@issp.u-tokyo.ac.jp>
2018-06-05 08:38:59 +0900

    update test

--- test/python/test_HeisenbergChain.py
+++ test/python/test_HeisenbergChain.py
@@ -13,16 +13,15 @@ def read_out(filename):
 class TestAtomic(unittest.TestCase):
     def test_HeisenbergChain(self):
         # run
-        #self.assertIs(0, os.system("../../src/mVMC/vmc.out -s %s/StdFace.def" %dir))
-        
+#        self.assertIs(0, os.system("../../src/mVMC/vmc.out -s %s/StdFace.def" %dir))        
         # get results
         array_calc = read_out("./output/zqp_opt.dat")[0:2]
         ref_ave = read_out("%s/ref/ref_mean.dat" %dir)[0:2]
         ref_std = read_out("%s/ref/ref_std.dat" %dir)[0:2]
-        testFalse = (ref_std < abs(array_calc - ref_ave))
-        print(abs(array_calc[testFalse]) < 1e-8)
-        print(abs(array_calc[testFalse]), abs(ref_ave[testFalse]), abs(ref_std[testFalse]))
-
+        testTrue = (ref_std > abs(array_calc - ref_ave))
+        testArray = abs(array_calc[testTrue] < 1e-8)
+        for _test in testArray:
+            self.assertTrue(_test)
         
 if __name__ == '__main__':
     unittest.main()
