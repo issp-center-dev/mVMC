d70bd0498f7df02a8ee24fcf046cdfb31572e4d7
RuQing Xu <rbh1998@mail.ustc.edu.cn>
2019-07-27 19:29:36 +0800

    Fixed a Miss in Changing Routine Name
    Change 59fe4ec

--- src/mVMC/vmccal.c
+++ src/mVMC/vmccal.c
@@ -934,10 +934,10 @@ void calculateQCACAQ_real(double *qcacaq, const double *lslca, const double w,
 
 }
 
-void calculateQCACAQ_real(double *qcacaq, const double *lslq, const double w,
-                          const int nLSHam, const int nCA, const int nCACA,
-                          int *eleIdx, int *eleCfg, int *eleNum, int *eleProjCnt,
-                          const double h1, const double ip) {
+void calculateQCACAQDC_real(double *qcacaq, const double *lslq, const double w,
+                            const int nLSHam, const int nCA, const int nCACA,
+                            int *eleIdx, int *eleCfg, int *eleNum, int *eleProjCnt,
+                            const double h1, const double ip) {
   const int n=nLSHam*nLSHam*nCACA;
   int rq,rp,ri,rj,rk,rl,s,t,idx;
   int i,tmp;
@@ -965,10 +965,10 @@ void calculateQCACAQ_real(double *qcacaq, const double *lslq, const double w,
   return;
 }
 
-void calculateQCACAQ(double complex *qcacaq, const double complex *lslq, const double w,
-                     const int nLSHam, const int nCA, const int nCACA,
-                     int *eleIdx, int *eleCfg, int *eleNum, int *eleProjCnt,
-                     const double complex h1, const double complex ip) {
+void calculateQCACAQCDC(double complex *qcacaq, const double complex *lslq, const double w,
+                        const int nLSHam, const int nCA, const int nCACA,
+                        int *eleIdx, int *eleCfg, int *eleNum, int *eleProjCnt,
+                        const double complex h1, const double complex ip) {
   const int n=nLSHam*nLSHam*nCACA;
   int rq,rp,ri,rj,rk,rl,s,t,idx;
   int i,tmp;
