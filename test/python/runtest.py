4d75e6cc4a92da999750973e5d568b32bdc8c4c2
Yuichi Motoyama <yomichi@tsg.jp>
2019-11-14 12:55:23 +0900

    clean up test

---
+++ test/python/runtest.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import os
+import subprocess
+import sys
+
+import numpy as np
+
+
+def read_out(filename):
+    # drop the first two columns
+    array = np.loadtxt(filename, dtype="float").astype("float")
+    return array
+
+
+if len(sys.argv) == 1:
+    print("usage: {} <model name>".format(sys.argv[0]))
+    sys.exit(-1)
+
+dir = os.path.join("data", sys.argv[1])
+result = subprocess.call(["../../src/mVMC/vmc.out", "-s", "%s/StdFace.def" % dir])
+if result != 0:
+    sys.exit(result)
+
+array_calc = read_out("./output/zqp_opt.dat")[0:2]
+ref_ave = read_out("%s/ref/ref_mean.dat" % dir)[0:2]
+ref_std = read_out("%s/ref/ref_std.dat" % dir)[0:2]
+
+result = 0
+for diff, s in zip(array_calc - ref_ave, ref_std):
+    diff = abs(diff)
+    if diff >= 3 * s and diff >= 1e-8:
+        result = -1
+
+sys.exit(result)
