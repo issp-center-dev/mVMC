ab32a3743a5ece510304495396116e9505e5747b
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2021-09-23 02:59:59 +0900

    Update Configs

---
+++ config/aocc.cmake
@@ -0,0 +1,16 @@
+# for AOCC
+
+set(CMAKE_C_COMPILER "clang" CACHE STRING "" FORCE)
+set(CMAKE_CXX_COMPILER "clang++" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "-g -O0 -Wall -Wformat -Werror=format-security")
+set(CMAKE_C_FLAGS_RELEASE "-Wno-unknown-pragmas -O3 -DNDEBUG -DHAVE_SSE2" CACHE STRING "" FORCE)
+
+set(CMAKE_Fortran_COMPILER "flang" CACHE STRING "" FORCE)
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -DHAVE_SSE2" CACHE STRING "" FORCE)
+
+# for AOCL
+set(BLA_VENDOR "FLAME" CACHE STRING "" FORCE)
+
+# for BLIS & block pfaffian
+set(BLIS_ARTIFACT_CONFIG "amd64")
+set(PFAFFIAN_BLOCKED ON)
