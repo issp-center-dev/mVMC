f1d8d62dbe2987d2c5e5917f9f77ddf65f33629a
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2021-09-23 03:10:09 +0900

    Fix Fujitsu Compiler Config Typo

--- config/fujitsu.cmake
+++ config/fujitsu.cmake
@@ -1,6 +1,6 @@
 # for Fujitsu Compiler
 set(CMAKE_C_COMPILER "mpifccpx" CACHE STRING "" FORCE)
-set(CMAKE_CXX_COMPILER "mpifCCpx" CACHE STRING "" FORCE)
+set(CMAKE_CXX_COMPILER "mpiFCCpx" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS_RELEASE "-DNDEBUG -Kfast,parallel -Kmemalias,alias_const" CACHE STRING "" FORCE)
 set(OpenMP_C_FLAGS "-Kopenmp" CACHE STRING "" FORCE)
 set(CMAKE_Fortran_COMPILER "mpifrtpx" CACHE STRING "" FORCE)
