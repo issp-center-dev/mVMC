5bae4b09d20bc0854ac5baa210b0b3e99255d2d9
Yuichi Motoyama <y-motoyama@issp.u-tokyo.ac.jp>
2021-09-23 23:39:37 +0900

    update dist.sh

--- dist.sh
+++ dist.sh
@@ -1,42 +1,40 @@
 #!/bin/sh
-#
-# #############  Note  ################################
-# Before packing, you should clean the GIT directory as
-# $ git clean -f -d -x
-# #####################################################
-#
-# Version ID
-#
+
+# #############  README  ##############
+# This makes an archive file including static copy of submodules (StdFace)
+# and PDF formatted documents docs/mVMC_(ja|en).pdf
+# The output filename is mVMC-${vid}.tar.gz,
+# where ${vid} is the version number such as 2.1.0 .
+# Before using this, install the following python packages:
+#   sphinx
+#   sphinx_numfig
+#   sphinxcontib_spelling
+#   git-archive-all
+# #####################################
+
+set -e
+
+# Retrieve Version ID
 major=`awk '$2=="VERSION_MAJOR"{print $3}' src/mVMC/include/version.h`
 minor=`awk '$2=="VERSION_MINOR"{print $3}' src/mVMC/include/version.h`
 patch=`awk '$2=="VERSION_PATCH"{print $3}' src/mVMC/include/version.h`
 vid=`echo ${major}.${minor}.${patch}`
-#
-mkdir mVMC-${vid}
-#
-cp -rf * mVMC-${vid}
-#
-# Build docments
-#
-cd mVMC-${vid}/doc/jp
-make -f makefile_doc_jp
-sed -i -e "s/mathjax/pngmath/g" conf.py
-make latexpdfja
-make html
-cd ../en
-sed -i -e "s/mathjax/pngmath/g" conf.py
-make latexpdfja
-make html
-cd ../../
-#
-# Remove some files
-#
-find ./ -name ".git*" -delete
-rm dist.sh
-rm -rf mVMC-${vid}
-#
-# Pack
-#
+
+# Build PDF docs
+rm -rf build-docs
+mkdir build-docs
+cd build-docs
+cmake -DDocument=ON ../
+make doc-ja-pdf
+make doc-en-pdf
+cp doc/ja/source/pdf/mVMC.pdf ../doc/mVMC-${vid}_ja.pdf
+cp doc/en/source/pdf/mVMC.pdf ../doc/mVMC-${vid}_en.pdf
 cd ../
-tar czvf mVMC-${vid}.tar.gz mVMC-${vid}
-rm -rf mVMC-${vid}
+rm -rf build-docs
+
+# Make archive
+git-archive-all \
+  --extra=doc/mVMC_ja.pdf \
+  --extra=doc/mVMC_en.pdf \
+  --prefix=mVMC-${vid} \
+  mVMC-${vid}.tar.gz
