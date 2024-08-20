ad4b8dcb98886385f0870a6842c84b8255f81a7c
k.ido <13773226+k-ido@users.noreply.github.com>
2024-08-20 16:09:36 +0900

    modify optimization level in gcc.cmake for ubuntu-22.04 workflow

--- config/gcc.cmake
+++ config/gcc.cmake
@@ -2,5 +2,5 @@
 set(CMAKE_C_COMPILER "gcc" CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER "g++" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS_DEBUG "-g -O0 -Wall  -Wformat -Werror=format-security")
-set(CMAKE_C_FLAGS_RELEASE "-O3 -Wno-unknown-pragmas ")
+set(CMAKE_C_FLAGS_RELEASE "-O2 -Wno-unknown-pragmas ")
 set(CMAKE_Fortran_COMPILER "gfortran" CACHE STRING "" FORCE)
