ca0a4acc8eb4cf5e7904d564dcec0d4aebf2f512
Takahiro Misawa <tmisawa@users.noreply.github.com>
2024-10-01 15:33:12 +0900

    Revert "Revert "Rebased "Separating Direct and Indirect Measurement of Two Body Green Function"""

--- src/mVMC/include/global.h
+++ src/mVMC/include/global.h
@@ -142,10 +142,9 @@ double *ParaQPOptTrans;
 
 /* for Green functions */
 int NCisAjs,         **CisAjsIdx;         /* [NCisAjs][3] */
-int NCisAjsCktAlt,   **CisAjsCktAltIdx;   /* [NCisAjsCktAlt][8] */
+int NCisAjsCktAlt,   **CisAjsCktAltIdx;   /* [NCisAjsCktAlt][2] */
 int NCisAjsCktAltDC, **CisAjsCktAltDCIdx; /* [NCisAjsCktAltDC][6] */
-int NCisAjsLz, **CisAjsLzIdx, **iOneBodyGIdx; /* For Lanczos method only for rank 0*/
-int NCisAjsCktAltLz, **CisAjsCktAltLzIdx;
+int **iOneBodyGIdx; /* For GF2 indirect measurement */
 
 /* Optimization flag */
 int *OptFlag; /* [NPara]  1: optimized, 0 or 2: fixed */
@@ -269,6 +268,7 @@ double complex *PhysCisAjs; /* [NCisAjs] */
 double complex *PhysCisAjsCktAlt; /* [NCisAjsCktAlt] */
 double complex *PhysCisAjsCktAltDC; /* [NCisAjsCktAltDC] */
 double complex *LocalCisAjs; /* [NCisAjs] */
+double complex *LocalCisAjsCktAltDC; /* [NCisAjsCktAltDC] */
 
 double complex Sztot,Sztot2; /* <Sz>,<Sz^2> */
 
@@ -286,10 +286,13 @@ double *LSLQ_real; /* [NLSHam][NLSHam]*/                      //TBC
 
 double complex *QCisAjsQ; /* QCisAjsQ[NLSHam][NLSHam][NCisAjs]*/ //TBC
 double complex *QCisAjsCktAltQ; /* QCisAjsCktAltQ[NLSHam][NLSHam][NCisAjsCktAlt]*/ //TBC
+double complex *QCisAjsCktAltQDC; /* QCisAjsCktAltQ[NLSHam][NLSHam][NCisAjsCktAlt] 
+                                     DC Lanczos Calculation */
 double complex *LSLCisAjs; /* [NLSHam][NCisAjs]*/                //TBC
 
 double *QCisAjsQ_real; /* QCisAjsQ[NLSHam][NLSHam][NCisAjs]*/ //TBC
 double *QCisAjsCktAltQ_real; /* QCisAjsCktAltQ[NLSHam][NLSHam][NCisAjsCktAlt]*/ //TBC
+double *QCisAjsCktAltQDC_real; /* QCisAjsCktAltQ[NLSHam][NLSHam][NCisAjsCktAlt]*/
 double *LSLCisAjs_real; /* [NLSHam][NCisAjs]*/                //TBC
 
 /***** Output File *****/
@@ -305,8 +308,10 @@ FILE *FileLS;
 FILE *FileLSQQQQ;
 FILE *FileLSQCisAjsQ;
 FILE *FileLSQCisAjsCktAltQ;
+FILE *FileLSQCisAjsCktAltQ;
 FILE *FileLSCisAjs;
 FILE *FileLSCisAjsCktAlt;
+FILE *FileLSCisAjsCktAltDC;
 
 
 /* FILE *FileTimerList; */
